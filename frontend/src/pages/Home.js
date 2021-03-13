af65ca33df0e949cc8fd076a5d50de83a90daf44
James Chiella <james.chiella@gmail.com>
2021-03-13 15:50:33 -0500

    basic home page design

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-import { Text } from '@chakra-ui/react';
+import { Container, Heading, Button, Flex, Text } from '@chakra-ui/react';
 
 export default function Home() {
-  return <Text>Hi there!</Text>;
+  const createActivity = () => {
+    alert('Hiya!');
+  };
+
+  return (
+    <Flex h="100vh" justify="center" align="center">
+      <Container textAlign="center">
+        <Heading size="4xl">Welcome to Eartrainr!</Heading>
+        <Text m="5">
+          Eartrainr is an easy way to create, share, and use simple, interactive
+          ear training activities.
+        </Text>
+        <Button colorScheme="blue" onClick={createActivity}>
+          Create an Activity
+        </Button>
+      </Container>
+    </Flex>
+  );
 }
