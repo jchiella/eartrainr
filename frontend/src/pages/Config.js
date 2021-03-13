ea7327100781fdcc808000b2452655bb48a4102c
James Chiella <james.chiella@gmail.com>
2021-03-13 18:30:25 -0500

    basic config page

--- frontend/src/pages/Config.js
+++ frontend/src/pages/Config.js
@@ -1,10 +1,106 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 
-import { Text } from '@chakra-ui/react';
+import {
+  Text,
+  Flex,
+  Heading,
+  Select,
+  Input,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Switch,
+  Spinner,
+  FormControl,
+  FormLabel,
+  Button,
+} from '@chakra-ui/react';
 
 export default function Config() {
   const { id } = useParams();
-  return <Text>Hi there, configuring {id}!</Text>;
+
+  const [activity, setActivity] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3003/activity/${id}`)
+      .then((res) => res.json())
+      .then(setActivity);
+  }, [id]);
+
+  const updateConfig = () => {
+    // TODO save configuration back to server
+  };
+
+  return activity === null ? (
+    <Spinner />
+  ) : (
+    <Flex
+      m="auto"
+      w="70ch"
+      h="100vh"
+      direction="column"
+      justify="center"
+      align="center"
+    >
+      <Heading size="2xl">Activity Options</Heading>
+      <Text m="1">Configure your activity here.</Text>
+      <Input
+        m="2"
+        placeholder="Activity Name"
+        value={activity.name}
+        onChange={(e) => setActivity({ ...activity, name: e.target.value })}
+      />
+      <Select
+        m="2"
+        placeholder="Activity Type"
+        value={activity.type}
+        onChange={(e) => setActivity({ ...activity, type: e.target.value })}
+      >
+        <option value="chord">Chord</option>
+        <option value="interval">Interval</option>
+        <option value="rhythm">Rhythm</option>
+      </Select>
+      <NumberInput
+        m="2"
+        w="100%"
+        min={1}
+        max={20}
+        value={activity.count !== 0 ? activity.count : ''}
+        onChange={(str) =>
+          setActivity({ ...activity, count: str.length ? parseInt(str) : 0 })
+        }
+      >
+        <NumberInputField placeholder="Number of Questions per Group" />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+      <FormControl display="flex" alignItems="center">
+        <FormLabel mb="0">Is the activity cumulative?</FormLabel>
+        <Switch
+          value={activity.cumulative}
+          onChange={(e) =>
+            setActivity({ ...activity, cumulative: e.target.value })
+          }
+        />
+      </FormControl>
+      {/* {activity.type === null ? (
+        <Spinner />
+      ) : activity.type === 'chord' ? (
+        <ChordGroupInput />
+      ) : activity.type === 'interval' ? (
+        <IntervalGroupInput />
+      ) : activity.type === 'rhythm' ? (
+        <RhythmGroupInput />
+      ) : null} */}
+      <Button colorScheme="blue" m="2" onClick={updateConfig}>
+        Update Options
+      </Button>
+    </Flex>
+  );
 }
